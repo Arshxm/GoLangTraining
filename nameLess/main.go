f45c1de1cb9d3a8106b3e78661c758dfa2fd9097
Arshxm <arshamreza1243@gmail.com>
2025-07-30 14:59:52 +0330

    nameless challenge is done

--- nameLess/main.go
+++ nameLess/main.go
@@ -1,18 +1,31 @@
 package main
 
 func AddElement(numbers *[]int, element int) {
-	append(*numbers, element)
+	*numbers = append(*numbers, element)
 }
 
 func FindMin(numbers *[]int) int {
-	//TODO
-	return 0
+	if len(*numbers) == 0 {
+		return 0
+	}
+	min := (*numbers)[0]
+	for _, number := range *numbers {
+		if number < min {
+			min = number
+		}
+	}
+	return min
 }
 
 func ReverseSlice(numbers *[]int) {
-	//TODO
+	for i := 0; i < len(*numbers)/2; i++ {
+		(*numbers)[i], (*numbers)[len(*numbers)-i-1] = (*numbers)[len(*numbers)-i-1], (*numbers)[i]
+	}
 }
 
 func SwapElements(numbers *[]int, i, j int) {
-	//TODO
+	if i < 0 || j < 0 || i >= len(*numbers) || j >= len(*numbers) {
+		return
+	}
+	(*numbers)[i], (*numbers)[j] = (*numbers)[j], (*numbers)[i]
 }
