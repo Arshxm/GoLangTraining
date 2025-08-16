58aafead92947e3fcc03d4cce4cea9740db5b2a2
Arshxm <arshamreza1243@gmail.com>
2025-08-16 08:45:29 +0330

    task interface is done

--- ToDo/task.go
+++ ToDo/task.go
@@ -1,6 +1,7 @@
 package qtodo
 
 import (
+	"errors"
 	"time"
 )
 
@@ -10,4 +11,52 @@ type Task interface {
 	GetAction() func()
 	GetName() string
 	GetDescription() string
+}
+
+type task struct {
+	action      func()
+	alarmTime   time.Time
+	name        string
+	description string
+}
+
+func (t *task) DoAction() {
+	t.action()
+}
+
+func (t *task) GetAlarmTime() time.Time {
+	return t.alarmTime
+}
+
+func (t *task) GetAction() func() {
+	return t.action
+}
+
+func (t *task) GetName() string {
+	return t.name
+}
+
+func (t *task) GetDescription() string {
+	return t.description
+}
+
+func NewTask(action func(), alarmTime time.Time, name string, description string) (*task, error) {
+	if action == nil {
+		return nil, errors.New("action cannot be nil")
+	}
+	if alarmTime.Before(time.Now()) {
+		return nil, errors.New("alarm time cannot be in the past")
+	}
+	if name == "" {
+		return nil, errors.New("name cannot be empty")
+	}
+	if description == "" {
+		return nil, errors.New("description cannot be empty")
+	}
+	return &task{
+		action:      action,
+		alarmTime:   alarmTime,
+		name:        name,
+		description: description,
+	}, nil
 }
