23f80a568f306e8947d3a6658fe7a006b4fbd8e1
Arshxm <arshamreza1243@gmail.com>
2025-09-11 19:24:08 +0330

    TLDR ready

---
+++ TLDR/main.go
@@ -0,0 +1,14 @@
+package main
+
+type TLDRDBCached struct {
+}
+
+func (t *TLDRDBCached) Retrieve(key string) string {
+}
+
+func (t *TLDRDBCached) List() []string {
+}
+
+func NewTLDRDBCached(nonCachedProvider TLDRProvider) TLDRProvider {
+	return nil
+}
