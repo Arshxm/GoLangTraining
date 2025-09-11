b80301b726eb2176f55fff50b774f4075bfe70d2
Arshxm <arshamreza1243@gmail.com>
2025-09-11 20:08:58 +0330

    tldr in progress

--- TLDR/main.go
+++ TLDR/main.go
@@ -1,12 +1,15 @@
 package main
 
 type TLDRDBCached struct {
+
 }
 
 func (t *TLDRDBCached) Retrieve(key string) string {
+	
 }
 
 func (t *TLDRDBCached) List() []string {
+
 }
 
 func NewTLDRDBCached(nonCachedProvider TLDRProvider) TLDRProvider {
